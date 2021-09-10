4136a1a3ec5fedd3b73a529cc42bc27792705401
Mateus Lima Fonseca <mateuslima90@gmail.com>
2021-09-10 13:29:58 -0300

    first commit

---
+++ src/main/kotlin/io/mkth/demowebfluxsecurity/DemoWebfluxSecurityApplication.kt
@@ -0,0 +1,11 @@
+package io.mkth.demowebfluxsecurity
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoWebfluxSecurityApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoWebfluxSecurityApplication>(*args)
+}
---
+++ src/test/kotlin/io/mkth/demowebfluxsecurity/DemoWebfluxSecurityApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.mkth.demowebfluxsecurity
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoWebfluxSecurityApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

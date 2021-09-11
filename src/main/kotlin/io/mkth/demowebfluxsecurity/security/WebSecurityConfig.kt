4d2f7228a05b54dc5d1dc2e203e3d8cafe8892d2
Mateus Lima Fonseca <mateuslima90@gmail.com>
2021-09-11 01:42:59 -0300

    fix the security config

--- src/main/kotlin/io/mkth/demowebfluxsecurity/security/WebSecurityConfig.kt
+++ src/main/kotlin/io/mkth/demowebfluxsecurity/security/WebSecurityConfig.kt
@@ -1,51 +1,49 @@
 package io.mkth.demowebfluxsecurity.security
 
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.server.SecurityWebFilterChain
-import org.springframework.web.reactive.config.EnableWebFlux
-import org.springframework.web.reactive.config.WebFluxConfigurer
 
-//@EnableWebFluxSecurity
-//@Configuration
-//class WebSecurityConfig {
-//
-//    @Bean
-//    fun configure(http: HttpSecurity) {
-//        http
-//                .authorizeRequests()
-//                .antMatchers("/", "/home").permitAll()
-//                .anyRequest().authenticated()
-//                .and()
-//                .formLogin()
-//                .loginPage("/login")
-//                .permitAll()
-//                .and()
-//                .logout()
-//                .permitAll();
-//    }
-//}
-//}
 
-@Configuration
-@EnableWebFlux
 @EnableWebFluxSecurity
-class KotlinSecurityConfiguration : WebFluxConfigurer {
-
+@EnableReactiveMethodSecurity
+class WebSecurityConfig {
+    
     @Bean
-    fun configureSecurity(http: ServerHttpSecurity): SecurityWebFilterChain {
-        return http
-                .httpBasic().disable()
-                .formLogin().disable()
-                .logout().disable()
-                .authorizeExchange()
-                .pathMatchers("/").permitAll()
-                .anyExchange().authenticated()
+    fun securitygWebFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain? {
+        return http.authorizeExchange()
+                .pathMatchers("/")
+                //.hasAuthority("ROLE_ADMIN")
+                .permitAll()
+                .anyExchange()
+                .authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .csrf()
+                .disable()
                 .build()
-
     }
 
+    @Bean
+    fun userDetailsService(): MapReactiveUserDetailsService? {
+        val user: UserDetails = User
+                .withUsername("user")
+                .password(passwordEncoder().encode("password"))
+                .roles("USER")
+                .build()
+        return MapReactiveUserDetailsService(user)
+    }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
 }
